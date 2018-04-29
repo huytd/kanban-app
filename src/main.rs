1143accc75c8c302d216feeb8cc3789a66dafb56
Huy Tran <kingbazoka@gmail.com>
2018-04-28 17:50:28 -0700

    Use spaces, no tabs

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ extern crate web_view;
 use web_view::*;
 
 fn main() {
-	  let size = (700, 400);
-	  let resizable = true;
-	  let debug = false;
-	  let titlebar_transparent = true;
-	  let frontend_cb = |_webview: &mut _, _arg: &_, _userdata: &mut _| {};
-	  let userdata = ();
+    let size = (700, 400);
+    let resizable = true;
+    let debug = false;
+    let titlebar_transparent = true;
+    let frontend_cb = |_webview: &mut _, _arg: &_, _userdata: &mut _| {};
+    let userdata = ();
 
     let html = format!(r#"
     <html>
         <head>
-        <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
         <style>{css}</style>
         </head>
         <body>
@@ -24,17 +24,17 @@ fn main() {
     css = r#"body { background: #1d1f21; }"#,
     js = include_str!("../www/dist.js"));
 
-	  run(
-		    "",
-		    Content::Html(html),
-		    Some(size),
-		    resizable,
-		    debug,
+    run(
+        "",
+        Content::Html(html),
+        Some(size),
+        resizable,
+        debug,
         titlebar_transparent,
-		    move |mut webview| {
+        move |mut webview| {
             webview.set_background_color(0.11, 0.12, 0.13, 1.0);
         },
-		    frontend_cb,
-		    userdata
-	  );
+        frontend_cb,
+        userdata
+    );
 }
